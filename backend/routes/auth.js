9666da55559e0f535a4e0f1cfcfc5c33ae7e832f
KiranK0304 <kirankuruvila03@gmail.com>
2025-10-15 13:34:16 +0530

    some more changes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,14 +8,12 @@ const {
   changePassword,
   deactivateAccount
 } = require('../controllers/authController');
-const { googleSignIn, googleLink } = require('../controllers/authController');
 const { protect } = require('../middleware/auth');
 const { validateRegister, validateLogin, validateUpdateProfile } = require('../middleware/validation');
 
 // Public routes
 router.post('/register', validateRegister, register);
 router.post('/login', validateLogin, login);
-router.post('/google', googleSignIn);
 
 // Protected routes
 router.use(protect); // All routes after this middleware are protected
@@ -24,6 +22,5 @@ router.get('/profile', getProfile);
 router.put('/profile', validateUpdateProfile, updateProfile);
 router.put('/change-password', changePassword);
 router.put('/deactivate', deactivateAccount);
-router.post('/google/link', googleLink);
 
 module.exports = router;
